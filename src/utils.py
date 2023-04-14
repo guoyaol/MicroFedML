208d5ecdc09b46c4514150688b4ad9412d53d6e0
guoyaol <guoyaoli00@gmail.com>
2023-04-14 18:46:56 +0000

    solved brute_force

--- src/utils.py
+++ src/utils.py
@@ -203,3 +203,101 @@ def split_dataset(training_dataset, transform, num_clients, num_shards, iid):
             for i in range(0, len(shard_inputs_sorted), shards_per_clients)
         ]
     return local_datasets
+
+
+def create_datasets_global(data_path, dataset_name, num_clients, num_shards, iid):
+    """Split the whole dataset in IID or non-IID manner for distributing to clients."""
+    dataset_name = dataset_name.upper()
+    # get dataset from torchvision.datasets if exists
+    if hasattr(torchvision.datasets, dataset_name):
+        # set transformation differently per dataset
+        if dataset_name in ["CIFAR10"]:
+            transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.ToTensor(),
+                    torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                ]
+            )
+        elif dataset_name in ["MNIST"]:
+            transform = torchvision.transforms.ToTensor()
+        
+        # prepare raw training & test datasets
+        training_dataset = torchvision.datasets.__dict__[dataset_name](
+            root=data_path,
+            train=True,
+            download=True,
+            transform=transform
+        )
+        test_dataset = torchvision.datasets.__dict__[dataset_name](
+            root=data_path,
+            train=False,
+            download=True,
+            transform=transform
+        )
+    else:
+        # dataset not found exception
+        error_message = f"...dataset \"{dataset_name}\" is not supported or cannot be found in TorchVision Datasets!"
+        raise AttributeError(error_message)
+
+    # unsqueeze channel dimension for grayscale image datasets
+    if training_dataset.data.ndim == 3: # convert to NxHxW -> NxHxWx1
+        training_dataset.data.unsqueeze_(3)
+    num_categories = np.unique(training_dataset.targets).shape[0]
+    
+    if "ndarray" not in str(type(training_dataset.data)):
+        training_dataset.data = np.asarray(training_dataset.data)
+    if "list" not in str(type(training_dataset.targets)):
+        training_dataset.targets = training_dataset.targets.tolist()
+    
+    # split dataset according to iid flag
+    if iid:
+        # shuffle data
+        shuffled_indices = torch.randperm(len(training_dataset))
+        training_inputs = training_dataset.data[shuffled_indices]
+        training_labels = torch.Tensor(training_dataset.targets)[shuffled_indices]
+
+        # partition data into num_clients
+        split_size = len(training_dataset) // num_clients
+        split_datasets = list(
+            zip(
+                torch.split(torch.Tensor(training_inputs), split_size),
+                torch.split(torch.Tensor(training_labels), split_size)
+            )
+        )
+
+        # finalize bunches of local datasets
+        local_datasets = [
+            CustomTensorDataset(local_dataset, transform=transform)
+            for local_dataset in split_datasets
+            ]
+    else:
+        # sort data by labels
+        sorted_indices = torch.argsort(torch.Tensor(training_dataset.targets))
+        training_inputs = training_dataset.data[sorted_indices]
+        training_labels = torch.Tensor(training_dataset.targets)[sorted_indices]
+
+        # partition data into shards first
+        shard_size = len(training_dataset) // num_shards #300
+        shard_inputs = list(torch.split(torch.Tensor(training_inputs), shard_size))
+        shard_labels = list(torch.split(torch.Tensor(training_labels), shard_size))
+
+        # sort the list to conveniently assign samples to each clients from at least two classes
+        shard_inputs_sorted, shard_labels_sorted = [], []
+        for i in range(num_shards // num_categories):
+            for j in range(0, ((num_shards // num_categories) * num_categories), (num_shards // num_categories)):
+                shard_inputs_sorted.append(shard_inputs[i + j])
+                shard_labels_sorted.append(shard_labels[i + j])
+                
+        # finalize local datasets by assigning shards to each client
+        shards_per_clients = num_shards // num_clients
+        local_datasets = [
+            CustomTensorDataset(
+                (
+                    torch.cat(shard_inputs_sorted[i:i + shards_per_clients]),
+                    torch.cat(shard_labels_sorted[i:i + shards_per_clients]).long()
+                ),
+                transform=transform
+            ) 
+            for i in range(0, len(shard_inputs_sorted), shards_per_clients)
+        ]
+    return local_datasets, test_dataset
