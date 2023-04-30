7dc397a4a791597acb7b54f16c7d03bc8b1424e9
guoyaol <guoyaoli00@gmail.com>
2023-04-30 00:41:15 +0000

    new docker tag

--- src/client_serverless/docker_push.py
+++ src/client_serverless/docker_push.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("docker build -t liugztat/q6993607-client_serverless:latest .")
+os.system("docker build -t liugztat/guoyao-client_serverless:latest .")
 
-os.system("docker tag q6993607-client_serverless docker.io/liugztat/q6993607-client_serverless")
+os.system("docker tag guoyao-client_serverless docker.io/liugztat/guoyao-client_serverless")
 
-os.system("docker push docker.io/liugztat/q6993607-client_serverless")
+os.system("docker push docker.io/liugztat/guoyao-client_serverless")
--- src/serverless/docker_push.py
+++ src/serverless/docker_push.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("docker build -t liugztat/q699-server_serverless:latest .")
+os.system("docker build -t liugztat/guoyao-server_serverless:latest .")
 
-os.system("docker tag q699-server_serverless docker.io/liugztat/q699-server_serverless")
+os.system("docker tag guoyao-server_serverless docker.io/liugztat/guoyao-server_serverless")
 
-os.system("docker push docker.io/liugztat/q699-server_serverless")
+os.system("docker push docker.io/liugztat/guoyao-server_serverless")
